e8bd1e9502bfe21ec3d6385939c85d8fa64e3ee2
Ville Valtonen <valtonen.vt@gmail.com>
2019-07-07 14:38:01 +0300

    renamed helloworld role main task

--- run-ansible-test.sh
+++ run-ansible-test.sh
@@ -9,7 +9,7 @@ docker build -t $DOCKER_IMAGE_NAME docker/
 docker run -ti --name $DOCKER_CONTAINER_NAME -d -p 32768:22  $DOCKER_IMAGE_NAME
 
 # running ansible playbook
-ansible-playbook -i ansible/environments/localtest/ ansible/myplaybook.yml -vvvv
+ansible-playbook -i ansible/environments/localtest/ ansible/myplaybook.yml -vvv
 
 # stopping and removing the container
 docker stop $DOCKER_CONTAINER_NAME && docker rm $DOCKER_CONTAINER_NAME
