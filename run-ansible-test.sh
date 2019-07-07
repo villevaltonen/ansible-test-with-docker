96e33c937fed9ffec2ad23008e923e6b146ecf47
Ville Valtonen <valtonen.vt@gmail.com>
2019-07-07 15:04:28 +0300

    made port to be an argument for script

--- run-ansible-test.sh
+++ run-ansible-test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # initializing variables
+EXPOSED_PORT=$1
 DOCKER_CONTAINER_NAME="ansible-test-centos"
 DOCKER_IMAGE_NAME="mycentos-ssh"
 
 # building and running the docker image
 docker build -t $DOCKER_IMAGE_NAME docker/
-docker run -ti --name $DOCKER_CONTAINER_NAME -d -p 32768:22  $DOCKER_IMAGE_NAME
+docker run -ti --name $DOCKER_CONTAINER_NAME -d -p $EXPOSED_PORT:22  $DOCKER_IMAGE_NAME
 
 # running ansible playbook
 ansible-playbook -i ansible/environments/localtest/ ansible/myplaybook.yml -vvv
