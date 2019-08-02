7e64680d6665344eff719854aadd58b857d0755b
Ville Valtonen <valtonen.vt@gmail.com>
2019-08-02 18:05:35 +0300

    removed verbose mode from playbook execution

--- run-ansible-test.sh
+++ run-ansible-test.sh
@@ -10,7 +10,7 @@ docker build -t $DOCKER_IMAGE_NAME docker/
 docker run -ti --name $DOCKER_CONTAINER_NAME -d -p $EXPOSED_PORT:22  $DOCKER_IMAGE_NAME
 
 # running ansible playbook
-ansible-playbook -i ansible/environments/localtest/ ansible/myplaybook.yml -vvv
+ansible-playbook -i ansible/environments/localtest/ ansible/myplaybook.yml
 
 # stopping and removing the container
 docker stop $DOCKER_CONTAINER_NAME && docker rm $DOCKER_CONTAINER_NAME
